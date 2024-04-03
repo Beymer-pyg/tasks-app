2da2dfa009424a89c893cf053126978cda748b72
Beymer Bustamante <bustamantef@unprg.edu.pe>
2024-04-03 04:05:31 -0500

    column and todoCard finish

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TodoCard from "./TodoCard";
+import { CirclePlus } from "lucide-react";
 
 type Props = {
   id: TypedColumn;
@@ -17,7 +18,7 @@ const idToColumnText: {
 
 const Column = ({ id, todos, index }: Props) => {
   return (
-    <div className="p-2 rounded-2xl shadow-sm bg-white/5">
+    <div className="p-2 rounded-2xl shadow-sm bg-violet-100/50">
       <h2 className="flex justify-between font-semibold text-xl">
         {idToColumnText[id]}
         <span className="text-gray-500 bg-gray-200 rounded-full px-2 py-1 text-sm font-normal">
@@ -28,6 +29,12 @@ const Column = ({ id, todos, index }: Props) => {
         {todos.map((todo, index) => (
           <TodoCard key={todo.$id} todo={todo} index={index} id={id} />
         ))}
+
+        <div className="flex items-end justify-end p-3">
+          <button className="text-green-500 hover:text-green-600">
+            <CirclePlus className="h-7 w-7" />
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,3 +1,5 @@
+import { CircleMinus } from "lucide-react";
+
 type Props = {
   todo: Todo;
   index: number;
@@ -5,7 +7,16 @@ type Props = {
 };
 
 function TodoCard({ todo, index, id }: Props) {
-  return <div>TodoCard</div>;
+  return (
+    <div className="bg-white rounded-md space-y-2 drop-shadow-md">
+      <div className="flex justify-between items-center p-4">
+        <p>{todo.title}</p>
+        <button className="text-red-600 hover:text-red-400">
+          <CircleMinus className="h-6 w-6" />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default TodoCard;
